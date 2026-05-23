c2ad3ef8a3f07715beac4bff4af56188d1e53adc
akshaypatel8989 <akshaypatel10.g@gmail.com>
2026-05-23 16:39:03 +0530

    port remove

--- index.js
+++ index.js
@@ -46,7 +46,7 @@
 
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000
+
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
